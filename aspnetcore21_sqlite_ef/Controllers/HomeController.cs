f4db544e77753326c5cd911186b861174408770f
Michell Cronberg <michell@cronberg.dk>
2018-09-14 10:03:53 +0200

    ajax

--- aspnetcore21_sqlite_ef/Controllers/HomeController.cs
+++ aspnetcore21_sqlite_ef/Controllers/HomeController.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using aspnetcore21_sqlite_ef.Models;
+﻿using aspnetcore21_sqlite_ef.Models;
 using aspnetcore21_sqlite_ef.Models.Configuration;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace aspnetcore21_sqlite_ef.Controllers
 {
@@ -23,7 +21,45 @@ namespace aspnetcore21_sqlite_ef.Controllers
             ViewBag.Kl = DateTime.Now.ToLongTimeString();
             ViewBag.Setting = appSetting.Setting2;
             using (PersonContext context = new PersonContext())
-                return View(context.Personer.ToList());            
+                return View(context.Personer.ToList());
         }
+
+        [HttpGet]
+        public IActionResult OpdaterPerson(int id)
+        {
+            try
+            {
+                using (PersonContext context = new PersonContext())
+                    return Json(context.Personer.Where(i => i.PersonId == id).First());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        
+        [HttpPost]
+        public IActionResult OpdaterPerson(Person p)
+        {
+            try
+            {
+                using (PersonContext context = new PersonContext())
+                {
+                    var tmp = context.Personer.Where(i => i.PersonId == p.PersonId).First();
+                    tmp.Navn = p.Navn;
+                    // opdater kun navn
+                    context.SaveChanges();
+                    return Json(new { personid = p.PersonId, opdateringOk = true });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
+
+        }
+
     }
 }
--- aspnetcore21_sqlite_ef/wwwroot/Script/app.js
+++ aspnetcore21_sqlite_ef/wwwroot/Script/app.js
@@ -1,7 +1,24 @@
-﻿(function () {    
+﻿(function () {
 
     $().ready(function () {
+
         clk();
+
+        // henter id=1
+        $.getJSON("/home/opdaterperson/1").done(function (d) {
+            $("#navn").val(d.navn);
+        }).fail(function (e) {
+            $("#fejl").html(e.responseText);
+        });
+
+        $("#btn").click(function () {
+            $.post("/home/opdaterperson", { personId: 1, navn: $("#navn").val() }).done(function () {
+                location.reload();
+            }).fail(function (e) {
+                $("#fejl").html(e.responseText);
+            });
+        });
+
         setInterval(function () {
             clk();
         }, 1000);
